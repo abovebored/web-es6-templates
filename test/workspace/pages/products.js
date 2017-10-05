cc3c25ea35fd3386ac052570bcb5bb0d1e13cda7
David Longworth <dave@abovebored.co.uk>
2017-10-05 17:36:35 +0100

    refactor: test template to es6

--- test/workspace/pages/products.js
+++ test/workspace/pages/products.js
@@ -3,7 +3,7 @@ ${partials_header}
 <h1>Products:</h1>
 
 <ul>
-  ${products.map(product => '<li>' + product.name + ' - £' + product.price + '</li>').join('\n')}
+  ${products.map(product => `<li>${product.name} - £${product.price}</li>`).join('\n')}
 </ul>
 
 ${partials_footer}
